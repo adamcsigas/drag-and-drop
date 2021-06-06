d297006371ad8c53f1327fd47f05159d64475338
adamcsigas <csigasadam@gmail.com>
2021-06-06 17:57:47 +0200

    make an autobind decorator do some interacting with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
 //we will use an OO approach
 
+//autobind decorator
+function autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      }
+    };
+    return adjustedDescriptor;
+}
+
 /*
 The goal here is to be able to:
   access the template
@@ -11,6 +28,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -20,11 +40,29 @@ class ProjectInput {
     //rendering by import template
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  //we need a listener
+  @autobind
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
   }
 }
 
